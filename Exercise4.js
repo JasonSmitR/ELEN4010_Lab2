ddcd5edf44848b4e155e095f8cf19c99b8abb71d
Jason <jason.smit@student.wits.ac.za>
2019-03-08 15:42:51 +0200

    Exercise 4

---
+++ Exercise4.js
@@ -0,0 +1,9 @@
+const convertFhToC = function (fh) {
+  return (fh - 32) * 5 / 9
+}
+const test1 = convertFhToC(32)
+console.log(test1)
+const test2 = convertFhToC(95)
+console.log(test2)
+const test3 = convertFhToC(-10)
+console.log(test3)
--- listing4.js
+++ listing4.js
@@ -5,3 +5,5 @@ const result = add(2, 3)
 console.log(result) // execute the function
 const anotherAdd = add
 console.log(anotherAdd(12, 4))// execute it again
+const result_2 = add(2, 4)
+console.log(result_2)
